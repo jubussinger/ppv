fbb6297be881c61cf4363680a522a36d6c311e7e
Júlia Bussinger <julia.bussingerveiga@hotmail.com>
2022-06-10 18:45:37 -0300

    migration

--- app/Http/Controllers/LancamentoImcController.php
+++ app/Http/Controllers/LancamentoImcController.php
@@ -6,5 +6,46 @@ use Illuminate\Http\Request;
 
 class LancamentoImcController extends Controller
 {
-    //
+    public function store(Request $request) {
+       
+        
+        switch($request->imc) {
+            case($request->imc < 18.5):
+ 
+                $abaixoPeso = true;
+ 
+                break;
+ 
+            case($request->imc <= 24.9):
+
+                $normal = true;
+    
+                break;
+            case($request->imc <= 29.9):
+
+                $sobrepeso = true;
+    
+                break;
+            case($request->imc <= 39.9):
+
+                $obeso = true;
+    
+                break;
+            case($request->imc > 40):
+
+                $muitoObeso = true;
+    
+                break;
+        }
+
+        $imc = LancamentoImc::create([
+            'abaixo_peso' => $abaixoPeso,
+            'normal' => $normal,
+            'sobrepeso' => $sobrepeso,
+            'obeso' => $obeso,
+            'muito_obeso' => $muitoObeso
+        ]);
+            
+
+     }
 }
