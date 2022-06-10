05c1e5057cdf46746336ee156a1c314678db54ad
Júlia Bussinger <julia.bussingerveiga@hotmail.com>
2022-06-10 18:06:15 -0300

    migration

--- database/migrations/2022_06_09_000008_create_turmas_table.php
+++ database/migrations/2022_06_09_000008_create_turmas_table.php
@@ -19,7 +19,9 @@ return new class extends Migration
             $table->string('horario_fim');
             $table->string('faixa_etaria');
             $table->unsignedBigInteger('categoria_id');
-            $table->foreign('categoria_id')->references('id')->on('categorias'); 
+            $table->foreign('categoria_id')->references('id')->on('categorias');            
+            $table->unsignedBigInteger('nucleo_id');
+            $table->foreign('nucleo_id')->references('id')->on('users'); 
             $table->timestamps();
         });
     }
